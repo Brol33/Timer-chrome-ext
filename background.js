d1c31b71b6aa097002e7804ba84d96e69c1fbb2d
John Lee <4685john@gmail.com>
2024-05-20 18:18:45 +1000

    started implementing timer functionality

--- background.js
+++ background.js
@@ -1,26 +1,35 @@
 var timerDuration;
 var startDate;
-var dueDate;
+var ringDate;
 var pauseDate;
+var timeLeft;
+var alarmSound = new Audio("Alarm01.wav");
 
-// duration in milliseconds
-function setTimer(duration) {
-  timerDuration = duration;
+function setTimer(durationMs) {
+  timerDuration = durationMs;
 }
 
-// Starts timer, duration in milliseconds
-function start(duration) {
+function startTimer(durationMs) {
+  ringDate = new Date();
+  var DurSeconds = parseInt((durationMs / 1000) % 60);
+  var DurMinutes = parseInt((durationMs / (1000 * 60)) % 60);
+  var DurHours = parseInt(durationMs / (1000 * 60 * 60));
+
+  ringDate.setHours(ringDate.getHours() + DurHours);
+  ringDate.setMinutes(ringDate.getMinutes() + DurMinutes);
+  ringDate.setSeconds(ring.getSeconds() + DurSeconds);
+
   startDate = new Date();
-  setInterval(updateTimer, 1000);
+  timeLeft = setTimeout(ring, ringDate - startDate)
+
+  //setInterval(updateDisplayTimer, 1000);
 }
 
-function updateTimer() {
-  timeLeft = timerDuration - (Date.now() - startDate.getTime());
-  setTimeout(ring, timeLeft)
+function updateDisplayTimer() {
 }
 
 function ring() {
-  
+  alarmSound.play();
 }
 
 function getTimeLeft() {
@@ -33,4 +42,8 @@ function pause() {
 
 function reset() {
   
+}
+
+function backgroundFunction () {
+  return "hello from the background!"
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
-function getTimeLeft() {
+var background = chrome.extension.getBackgroundPage();
 
+document.getElementById("start").addEventListener("click", start)
+
+function start() {
+  background.startTimer(1000)
 }
 
 function getTimeLeft() {
   
 }
+
+console.log(background)
